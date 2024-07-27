4b6cabec861f1415e258ebf56ea1f01482092cac
Ryhegit <122738539+AgittSahin@users.noreply.github.com>
2024-07-27 17:18:49 +0200

    Little fix for input

--- pizza_delivery.py
+++ pizza_delivery.py
@@ -1,7 +1,7 @@
 print("Thank you for choosing Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L ").upper()
+add_pepperoni = input("Do you want pepperoni? Y or N ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N ").upper()
 
 bill = 0
 
