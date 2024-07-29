a80e6afb3f35a6f9cb46d551a613cba90ecfb1f3
Ryhegit <122738539+AgittSahin@users.noreply.github.com>
2024-07-29 03:36:53 +0200

    range function improvement

---
+++ hangman.py
@@ -0,0 +1,27 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+
+for letter in chosen_word:
+    display += "_"
+    
+guess = input("Guess a letter: ").lower()
+
+print(display)
+
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+
+for letter in range(len(chosen_word)):
+    if letter == guess:
+        letter =
+    
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
--- range.py
+++ range.py
@@ -1,8 +1,25 @@
-target = int(input())
+#print every number in the list
+my_list1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-sum = 0
+length_of_mylist1 = len(my_list1)
 
-for number in range(0,target+1,2):
-  sum += number
+for number in range(length_of_mylist1):
+  print(number)
 
-print(sum)
+#print every index and letter in a list
+my_list2 = ['a','b','c','d','e','f'',g','h','i','j','k','l','m','n','o','p','q','r','s','t','y','z']
+
+for index in range(len(my_list2)):
+  print(index, my_list2[index])
+  
+#print every index and letter in a word
+word = "ScienceFiction"
+
+for index in range(len(word)):
+  print(index, word[index])
+  
+#print evey letter of word taken fron end to beginning
+my_word = input("What is your word? ")
+
+for index in range(len(my_word)  -1,-1,-1):
+  print(my_word[index])
